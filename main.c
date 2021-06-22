221cdf0a7e9bb9f4346be2c7df5a45d36aa73475
Seungbo An <seuan@c7r2s1.42seoul.kr>
2021-06-22 15:10:04 +0900

    Add image making

--- main.c
+++ main.c
@@ -17,6 +17,9 @@
 # define KEY_S			1
 # define KEY_D			2
 
+// IMG
+# define IMG_WIDTH 500
+# define IMG_HEIGHT 500
 //Since key_press() can recieve only one argument, all the argument shold be gathered in one structure
 //x,y and str are meaningless variables.
 typedef struct s_param{
@@ -24,6 +27,17 @@ typedef struct s_param{
 	int		y;
 	// char	str[3];
 }				t_param;
+
+typedef struct	s_img
+{
+	void		*img_ptr;
+	int			*data;
+	//아래 3개 값은 이해 안해도 사용하는데 지장이 없음. 
+	//선언한뒤 함수의 인자로만 잘 넣어주면 알아서 정보를 받아나옴.
+	int			size_l;
+	int			bpp;
+	int			endian;
+}				t_img;
 
 //Only param->x will be used. 
 void			param_init(t_param *param)
@@ -70,14 +84,33 @@ int main()
     void 	*win_ptr;
     int		width;
     int		height;
+    int		count_w;
+	int		count_h;
     t_param param;
+    t_img   img;
     
-    width = 400;
-    height = 400;
+    width = 600;
+    height = 600;
     param_init(&param);
     mlx_ptr = mlx_init();	//mlx 할당 실패할 경우 NULL반환
     win_ptr = mlx_new_window(mlx_ptr, width, height, "./so_long"); 
     				// 할당된 mlx에 너비. 높이를 갖는 윈도우 창 생성.
+    img.img_ptr = mlx_new_image(mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
+	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bpp, &img.size_l, &img.endian);
+
+    count_h = -1;
+	while (++count_h < IMG_HEIGHT)
+	{
+		count_w = -1;
+		while (++count_w < IMG_WIDTH)
+		{
+			if (count_w % 2)
+				img.data[count_h * IMG_WIDTH + count_w] = 0xFFFFFF;
+			else
+				img.data[count_h * IMG_WIDTH + count_w] = 0xFF0000;
+		}
+	}
+    mlx_put_image_to_window(mlx_ptr, win_ptr, img.img_ptr, 0, 0);
     mlx_hook(win_ptr, X_EVENT_KEY_PRESS, 0, &key_press, &param);
     mlx_loop(mlx_ptr);
 }
